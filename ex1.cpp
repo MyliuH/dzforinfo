1239719c11bde8fe585cf8b19e689c28acaee158
MyliuH <138070641+MyliuH@users.noreply.github.com>
2026-03-13 11:07:39 +0300

    Homework

---
+++ ex1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+typedef int(*func)(int);
+
+int SevenPlus(int num) {
+    return num + 7;
+}
+
+int SevenMultiplie(int num) {
+    return num * 7;
+}
+
+void map(int* A, size_t size, func f) {
+
+    for (size_t i = 0; i < size; i++) {
+        std::cout << f(A[i]) << " ";
+    }
+    std::cout << "\n";
+}
+
+int main()
+{
+    int A[10];
+
+    for (size_t i = 0; i < std::size(A); i++) {
+        A[i] = i;
+        std::cout << A[i] << " ";
+    }
+    std::cout << "\n";
+
+    map(A, std::size(A), SevenPlus);
+
+    map(A, std::size(A), SevenMultiplie);
+}
+
---
+++ ex2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <locale>
+
+int cadiou(int x, int y, int z) 
+{
+    if (x == y) 
+    {
+        return z;
+    }
+    else if (x > y) 
+    {
+        cadiou(x, y + 1, z * (y + 1));
+    }
+    else if (x < y)
+    {
+        std::cout << "Ошибка: X не может быть меньше Y.";
+    }
+}
+
+float CalculationPi(int k, int N) 
+{
+    if (k > N) 
+    {
+        return 0;
+    }
+
+    return pow(-1, k) / pow((k + 1), 2) + CalculationPi(k + 1, N);
+    
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int x, y, z;
+
+    std::cout << "Часть A: \n";
+    std::cout << "Введите x, y, z: ";
+    std::cin >> x >> y >> z;
+    std::cout << "Результат: " << cadiou(x,y,z) << "\n";
+
+
+    int N;
+
+    std::cout << "Часть B: \n";
+    std::cout << "Введите N: ";
+    std::cin >> N;
+    std::cout << "Результат Pi = " << sqrt(12*CalculationPi(0, N));
+
+}
